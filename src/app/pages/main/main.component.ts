eab7cc3c25c4983e3ba748b7e3aaad2c77f95cce
Sergey Malyuga <sergey@malyuga.ru>
2026-02-16 16:16:06 +0300

    feat(header.component): добавить логотип

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {HeroComponent} from '../../features/hero/hero.component';
+import {HeaderComponent} from '../../features/header/header.component';
 
 @Component({
   selector: 'app-main',
   imports: [
-    HeroComponent
+    HeroComponent,
+    HeaderComponent
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
