902bbd7f8d39befc730b5855730429ae2bd30891
Sergey Malyuga <sergey@malyuga.ru>
2026-02-20 10:49:29 +0300

    feat(projects.component): создать компонент и подключить его к main.component

---
+++ src/app/features/projects/projects.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  imports: [],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProjectsComponent {
+
+}
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeroComponent } from '../../features/hero/hero.component';
 import { HeaderComponent } from '../../features/header/header.component';
 import { AboutComponent } from '../../features/about/about.component';
+import {ProjectsComponent} from '../../features/projects/projects.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HeroComponent, HeaderComponent, AboutComponent],
+  imports: [HeroComponent, HeaderComponent, AboutComponent, ProjectsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
