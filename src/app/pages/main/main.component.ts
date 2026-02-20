ef05254be097230bf3dae92492cfbd5a25fee94e
Sergey Malyuga <sergey@malyuga.ru>
2026-02-20 13:12:37 +0300

    feat(footer.component): создать компонент

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -3,10 +3,11 @@ import { HeroComponent } from '../../features/hero/hero.component';
 import { HeaderComponent } from '../../features/header/header.component';
 import { AboutComponent } from '../../features/about/about.component';
 import {ProjectsComponent} from '../../features/projects/projects.component';
+import {FooterComponent} from '../../features/footer/footer.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HeroComponent, HeaderComponent, AboutComponent, ProjectsComponent],
+  imports: [HeroComponent, HeaderComponent, AboutComponent, ProjectsComponent, FooterComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
